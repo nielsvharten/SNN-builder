ee2af29179f5a22da4591fac6fc8cf8ba68d1449
Niels van Harten <niels@vharten.com>
2023-03-30 16:47:49 +0200

    Clean API

--- backend/api.py
+++ backend/api.py
@@ -10,29 +10,13 @@ from core.nodes import LIF
 from core.simulators import Simulator
 
 app = Flask(__name__)
-#cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
-
-def remove_json_keys(json, keys):
-    for key in keys:
-        json.pop(key)
-
-    return json
-
-@app.route('/time')
-def get_current_time():
-    return {'time': time.time()}
 
 @app.route('/network', methods=['POST'])
 @cross_origin()
 def network():
-    network = request.get_json()
-    net = Network()
-
-    read_out_nodes = []
-    nodes = {}
-    for node in network['nodes']:
+    def create_node(node):
         id = node['id']
         if node['type'] == "lif":
             m = float(node['m'])
@@ -44,27 +28,37 @@ def network():
             I_e = float(node['I_e'])
             noise = float(node['noise'])
             
-            nodes[id] = net.createLIF(ID=id, m=m, V_init=V_init, V_reset=V_reset, V_min=V_min, thr=thr, amplitude=amplitude, I_e=I_e, noise=noise)
+            return net.createLIF(ID=id, m=m, V_init=V_init, V_reset=V_reset, V_min=V_min, thr=thr, amplitude=amplitude, I_e=I_e, noise=noise)
         elif node['type'] == "input":
             train = ast.literal_eval(node['train'])
             loop = bool(node['loop'])
 
-            nodes[id] = net.createInputTrain(ID=id, train=train, loop=loop)
+            return net.createInputTrain(ID=id, train=train, loop=loop)
         elif node['type'] == "random":
             p = float(node['p'])
             amplitude = float(node['amplitude'])
 
-            nodes[id] = net.createRandomSpiker(ID=id, p=p, amplitude=amplitude)
-
-        #if bool(node['read_out']):
-        read_out_nodes.append(nodes[id])
-
-    for synapse in network['synapses']:
+            return net.createRandomSpiker(ID=id, p=p, amplitude=amplitude)    
+    
+    def create_synapse(synapse):
         pre = nodes[synapse['pre']]
         post = nodes[synapse['post']]
         w = float(synapse['w'])
         d = int(synapse['d'])
-        net.createSynapse(pre=pre, post=post, w=w, d=d)
+        return net.createSynapse(pre=pre, post=post, w=w, d=d)
+
+    network = request.get_json()
+    net = Network()
+
+    read_out_nodes = []
+    nodes = {}
+    for json_node in network['nodes']:
+        node = create_node(json_node)
+        nodes[node.ID] = node
+        read_out_nodes.append(node)
+
+    for json_synapse in network['synapses']:
+        create_synapse(json_synapse)
 
     sim = Simulator(net)
     
@@ -86,11 +80,10 @@ def network():
     measurements = {  }
     for i, node in enumerate(read_out_nodes):
         measurements[node.ID] = { "spikes": [], "voltages": []}
-
         for spike in spikes:
             measurements[node.ID]["spikes"].append(str(spike[i]).lower())
         
         for voltage in voltages:
             measurements[node.ID]["voltages"].append(voltage[i])
 
-    return jsonify({"timeStep": 0, "duration": duration, "measurements": measurements})
+    return jsonify({"duration": duration, "measurements": measurements})
