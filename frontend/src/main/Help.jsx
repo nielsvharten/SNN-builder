cff36043ad21f3ebc378dd06fac471f50257990b
Niels van Harten <niels@vharten.com>
2023-04-17 11:10:04 +0200

    Update Help.jsx

--- frontend/src/main/Help.jsx
+++ frontend/src/main/Help.jsx
@@ -6,19 +6,37 @@ import Modal from "react-bootstrap/Modal";
 class Help extends Component {
   render() {
     const { keyMap, show, onClose } = this.props;
-    console.log(keyMap);
+    const labels = {
+      ADD_LIF: "Insert LIF neuron",
+      ADD_INPUT: "Insert Input Train",
+      ADD_RANDOM: "Insert Random Spiker",
+      CONNECT_MODE: "Connect this node to another",
+      DELETE_SELECTED: "Delete selected node/synapse",
+      UNDO: "Undo last action",
+      REDO: "Redo last action",
+      EXECUTE: "Execute network",
+      EDIT: "Edit network",
+    };
+
     return (
       <Modal show={show} onHide={onClose}>
         <Modal.Header closeButton>
-          <Modal.Title>Hotkeys</Modal.Title>
+          <Modal.Title>Help</Modal.Title>
         </Modal.Header>
         <Modal.Body>
           <Form>
+            <h5>Global hotkeys</h5>
             {Object.entries(keyMap).map(([key, value]) => (
               <Form.Group>
-                {key}: <b>{value}</b>
+                {labels[key]}: <b>{value}</b>
               </Form.Group>
             ))}
+            <br />
+            <h5>Other functionality</h5>
+            <p>
+              Insert infinity as property value if applicable using
+              <b> ctrl+shift+8</b>
+            </p>
           </Form>
         </Modal.Body>
         <Modal.Footer>
